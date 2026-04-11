9ccfb847233af4896a2c0b27cfc51be4a8226cb4
Lautaro Mendieta <lemendieta96@gmail.com>
2026-04-11 19:58:24 -0300

    Implement log level selection

--- src/logger.c
+++ src/logger.c
@@ -5,5 +5,24 @@
 void Logger_init(LoggerLevel_t level) {
   ZephyrLogger_init();
   ULOG_INIT();
-  ULOG_SUBSCRIBE(ZephyrLogger_send, ULOG_DEBUG_LEVEL);
+
+  ulog_level_t ulogLevel;
+  switch (level) {
+    case LOGGER_LEVEL_DEBUG:
+      ulogLevel = ULOG_DEBUG_LEVEL;
+      break;
+    case LOGGER_LEVEL_INFO:
+      ulogLevel = ULOG_INFO_LEVEL;
+      break;
+    case LOGGER_LEVEL_WARN:
+      ulogLevel = ULOG_WARNING_LEVEL;
+      break;
+    case LOGGER_LEVEL_ERROR:
+      ulogLevel = ULOG_ERROR_LEVEL;
+      break;
+    default:
+      ulogLevel = ULOG_DEBUG_LEVEL;
+  }
+
+  ULOG_SUBSCRIBE(ZephyrLogger_send, ulogLevel);
 }
